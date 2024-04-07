73e5a7ce6f77375a580a6d996403db014931e401
mrlootbox <mrfrontdr@gmail.com>
2024-04-08 00:18:48 +0530

    add function to cart

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-const cart = [];
+export const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function saveLocal() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+    const existingItemIndex = cart.findIndex((item) => item.productId === productId);
+
+    if (existingItemIndex !== -1) {
+        cart[existingItemIndex].quantity++;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+    saveLocal();
+}
+
